7612f4c5f70504d2600464e364c09ea3d53f0a04
Marina Delneri <mgdelneri@gmail.com>
2021-02-25 13:27:02 -0600

    Researched and added functions that allow for choosing random criteria choices

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +13,37 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+//Functions for generating random criteria
+
+//Random LowerCase
+function getRandomLowerCase() {
+  return String.fromCharCode(Math.floor(Math.random()*26)+97);
+};
+
+console.log(getRandomLowerCase());
+
+//Random UpperCase
+function getRandomUpperCase() {
+  return String.fromCharCode(Math.floor(Math.random()*26)+65)
+};
+
+console.log(getRandomUpperCase());
+
+//Random Number
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random()*10)+48)
+};
+
+console.log(getRandomNumber());
+
+//Random SpecialCharacters
+function getRandomSymbol() {
+  var symbol = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~";
+  return symbol[Math.floor(Math.random()*symbol.length)];
+}
+
+console.log(getRandomSymbol());
