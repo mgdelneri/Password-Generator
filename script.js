1a3e1b3c848f3df32203acc208534f44e17ab9bf
Marina Delneri <mgdelneri@gmail.com>
2021-02-26 11:12:38 -0600

    Created the first few prompts for character-type user choices and started adding if statements for the answers

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Prompting and choosing
+var criteria = function() {
+  //Ask user what criteria they want for their password
+  var lengthChoice = window.prompt("How long would you like your password to be?\nPick between 8 and 128 characters.");
+
+  if (lengthChoice < 8 || lengthChoice > 128) {
+    window.alert("Invalid length.");
+    //? How to go back to lengthChoice prompt?//
+  }
+
+  //? This goes with the Random Functions!!//
+  if (lengthChoice >= 8 && lengthChoice <= 128) {
+    //? Where is this info stored so it can be used to generate password later?//
+  }
+
+  var uppercaseChoice = window.prompt("Would you like Uppercase letters in your password?\nAnswer: Y or N");
+
+  if (!uppercaseChoice) {
+    window.alert("Please answer Y or N");
+    //? How to go back to uppercaseChoice?//
+  }
+  //? If == Y/N statements?//
+
+  var lowercaseChoice = window.prompt("Would you like lowercase letters in your password?\nAnswer: Y or N")
+
+  if (!lowercaseChoice) {
+    window.alert("Please answer Y or N");
+  }
+}
+
+criteria();
+
 
 // Write password to the #password input
 function writePassword() {
@@ -17,7 +49,7 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-//Functions for generating random criteria
+/*//Functions for generating random criteria
 
 //Random LowerCase
 function getRandomLowerCase() {
@@ -46,4 +78,4 @@ function getRandomSymbol() {
   return symbol[Math.floor(Math.random()*symbol.length)];
 }
 
-console.log(getRandomSymbol());
+console.log(getRandomSymbol());*/
