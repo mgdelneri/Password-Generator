675aada6ef586cb8dda7b0a63b83f43861f6abdd
Marina Delneri <mgdelneri@gmail.com>
2021-02-28 11:59:30 -0600

    Changing structure and logic of JavaScript code/Introduce random choice functions with CharCodes

--- script.js
+++ script.js
@@ -4,13 +4,6 @@ var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// Array elements for different categories of character options
-var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numericChoice = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialCharacChoice = ["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", "."];
-console.log(numericChoice);
-console.log(specialCharacChoice);
 // Prompting and choosing criteria for the password
 function generatePassword() {
   //Prompt asking the user their desired password length
@@ -22,6 +15,9 @@ function generatePassword() {
     return;
   }
 
+  // Turning the type of the lengthChoice from "string" to "number"
+  var lengthChoice = +lengthChoice.value;
+  
   // Prompt giving user the choice of whether they want Uppercase letters in their password
   var uppercaseChoice = window.confirm("Would you like Uppercase letters included in your password?\n'OK' = Yes\n'Cancel' = No");
 
@@ -34,29 +30,10 @@ function generatePassword() {
   // Prompt giving user the choice of whether they want special characters in their password
   var specialCharacChoice = window.confirm("Would you like special characters included in your password?\n'OK' = Yes\n'Cancel' = No");
 
-  // Create a "basket" where all of the possible characters go into
-  var basket = []
-  
-  // Throw characters in the "basket" if user choice is "true" for each character category
-  if (uppercaseChoice) {
-    basket = basket.concat(uppercaseLetters);
-  }
-
-  if (lowercaseChoice) {
-    basket = basket.concat(lowercaseLetters);
-  }
-
-  if (numericChoice) {
-    basket = basket.concat(numericChoice);
-  }
-
-  if (specialCharacChoice) {
-    basket = basket.concat(specialCharacChoice);
-  }
-
-  console.log(basket);
-  
-  return"password";
+  /* Loop 
+  for (var i = 0; i < lengthChoice; i++) {
+    if 
+  }*/
 }
 
 // Write password to the #password input
@@ -66,3 +43,67 @@ function writePassword() {
 
   passwordText.value = password;
 }
+
+// Functions picking random characters
+
+// Pick a random Uppercase letter
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+console.log(getRandomUpper());
+
+// Pick a random Lowercase letter
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() *26) + 97);
+}
+
+console.log(getRandomLower());
+
+// Pick a random number
+function getRandomNumeric() {
+  return String.fromCharCode(Math.floor(Math.random() *10) +48);
+}
+
+console.log(getRandomNumeric());
+
+// Pick a random special character
+function getRandomSpecCharac() {
+  var symbol = "!@#$%^&*()[]=<>/,.|~?";
+  return symbol[Math.floor(Math.random() * symbol.length)];
+}
+
+console.log(getRandomSpecCharac());
+
+
+
+
+
+/*
+// Array elements for different categories of character options
+var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacArray = ["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", "."];
+
+
+// Create a "basket" where all of the possible characters go into
+var basket = []
+  
+// Throw characters in the "basket" if user choice is "true" for each character category
+if (uppercaseChoice) {
+  basket = basket.concat(uppercaseArray);
+}
+
+if (lowercaseChoice) {
+  basket = basket.concat(lowercaseArray);
+}
+
+if (numericChoice) {
+  basket = basket.concat(numericArray);
+}
+
+if (specialCharacChoice) {
+  basket = basket.concat(specialCharacArray);
+}
+*/
