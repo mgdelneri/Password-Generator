2b0fd356c9aa50b01c9a462c3bf1de7b0e167fa1
Marina Delneri <mgdelneri@gmail.com>
2021-03-01 12:45:54 -0600

    Random edits from study group; slight change in logic of JavaScript

--- script.js
+++ script.js
@@ -4,19 +4,24 @@ var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+var uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numericArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacArray = ["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", "."];
+
 // Prompting and choosing criteria for the password
 function generatePassword() {
   //Prompt asking the user their desired password length
   var lengthChoice = window.prompt("How long would you like your password to be?\nIt can be from 8 to 128 characters.");
   
+  // Turning the type of the lengthChoice from "string" to "number"
+  lengthChoice = +lengthChoice.value;
+
   if (lengthChoice < 8 || lengthChoice > 128) {
     window.alert("Invalid length.");
     generatePassword();
     return;
   }
-
-  // Turning the type of the lengthChoice from "string" to "number"
-  lengthChoice = +lengthChoice.value;
   
   // Prompt giving user the choice of whether they want Uppercase letters in their password
   var uppercaseChoice = window.confirm("Would you like Uppercase letters included in your password?\n'OK' = Yes\n'Cancel' = No");
@@ -30,8 +35,36 @@ function generatePassword() {
   // Prompt giving user the choice of whether they want special characters in their password
   var specialCharacChoice = window.confirm("Would you like special characters included in your password?\n'OK' = Yes\n'Cancel' = No");
 
-  // Functions picking random characters
-  // Pick a random Uppercase letter 
+
+  var basket = []
+  // Loop dependent on desired length of password, followed by if statements for each type of character, to be picked randomly
+  for (var i = 0; i < lengthChoice; i += 0) {
+
+    // If special characters included in the password, pick them randomly from the special character array
+    if (uppercaseChoice === true) {
+      var uppercaseArrayIndex = Math.floor(Math.random() * (uppercaseArray.length - 1));
+      i++;
+      if (i >= lengthChoice) {
+        break
+      };
+      basket.push(uppercaseArray[uppercaseArrayIndex]);
+    }
+
+    // If special characters included in the password, pick them randomly from the special character array
+    if (specialCharacChoice === true) {
+      var specialCharacArrayIndex = Math.floor(Math.random() * (specialCharacArray.length - 1));
+      i++;
+      if (i >= lengthChoice) {
+        break
+      };
+      basket.push(specialCharacArray[specialCharacArrayIndex]);
+    }
+  }
+
+  
+
+
+
     function getRandomUpper() {
     if (uppercaseChoice) {
       return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
@@ -67,16 +100,9 @@ function generatePassword() {
 
   // Array of random-choices functions 
   var arrayRandomChoices = [getRandomUpper(), getRandomLower(), getRandomNumeric(), getRandomSpecCharac()];
-  
-  // TODO: Loop that runs as many times as the lengthChoice. What runs inside of it? -> the picking of the random characters of desired types
-  for (var i = 0; i < lengthChoice; i++) {
-    /*if (lengthChoice > arrayRandomChoice.length) {
-      set [i] = 0;
-    }*/
-    console.log(arrayRandomChoices[i]);
-  }
 
   // TODO: Connect the result of password generation to the text field 
+  // String version of basket array: passwordArray.join("")
 
 }
 
@@ -85,7 +111,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = //password;
 }
 
 
